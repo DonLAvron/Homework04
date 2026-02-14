1774d318cf8f6cc68b90820f284a0c5e38300c36
User <immolateimproved89@gmail.com>
2026-02-14 16:33:45 +0300

    HM-04

--- src/Main.java
+++ src/Main.java
@@ -40,9 +40,9 @@ public class Main {
         int deliveryDistance = 95;
         if (deliveryDistance > 0 && deliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1" + "\n");
-        } else if (deliveryDistance > 0 && deliveryDistance <= 60) {
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
             System.out.println("Потребуется дней: 2" + "\n");
-        } else if (deliveryDistance > 0 && deliveryDistance <= 100) {
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: 3" + "\n");
         } else {
             System.out.println("Доставки нет" + "\n");
